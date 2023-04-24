13a12e5f24c4390933e34aef38ce744b6942a1cb
ule270 <u.le270@gmail.com>
2023-04-24 09:10:31 -0400

    took out comments in html

--- index.ts
+++ index.ts
@@ -3,13 +3,28 @@ import './style.css';
 
 
 const form: HTMLFormElement = document.querySelector('#defineform');
+// holds baseURL for Dictionary API
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/';
 
-
-form.onsubmit = () => {
+//async function handles the form 
+form.onsubmit = async () => {
   const formData = new FormData(form);
-
-  console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
+  // gets the first word user entered
+
+  try {
+    // fetch to send request to Dictionary API
+    const response = await fetch(API_URL + text);
+    // await to parse the response body as JSON
+    // and log to console
+    const data = await response.json(); 
+    console.log(data);
+  } catch (error) {
+    // handles any errors during API request 
+    console.error(error); 
+  }
+
+  
   return false; // prevent reload
 };
